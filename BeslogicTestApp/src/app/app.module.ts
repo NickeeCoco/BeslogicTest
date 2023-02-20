2ed2921aff192b4c527625d980ec0d44f2e22f9c
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-20 16:43:58 -0500

    Add Http Client Module

--- BeslogicTestApp/src/app/app.module.ts
+++ BeslogicTestApp/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { RentalsComponent } from './rentals/rentals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RentalsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
