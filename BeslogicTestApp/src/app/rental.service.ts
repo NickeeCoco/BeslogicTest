c8f8870dcb91e083e82eb3ee56a510d0dfdcd426
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-20 16:43:43 -0500

    Generate rental service
    Get http requests

---
+++ BeslogicTestApp/src/app/rental.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Rental } from './Rental';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RentalService {
+  private apiServerUrl = 'http://localhost:8080/rental';
+
+  constructor(private http: HttpClient) {};
+
+  public getRentals(): Observable<Rental[]> {
+    return this.http.get<Rental[]>(`${this.apiServerUrl}/all`);
+  }
+
+  public getRentalById(id: number): Observable<Rental> {
+    return this.http.get<Rental>(`${this.apiServerUrl}/find/${id}`)
+  }
+}
