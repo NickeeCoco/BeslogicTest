cdb47c2aa8e6cf913b6b9b612ecd5912d7d24c7c
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-20 14:44:19 -0500

    Initialize backend

---
+++ BeslogicTest/src/main/java/com/nickeecoco/beslogictest/BeslogictestApplication.java
@@ -0,0 +1,13 @@
+package com.nickeecoco.beslogictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeslogictestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeslogictestApplication.class, args);
+	}
+
+}
---
+++ BeslogicTest/src/test/java/com/nickeecoco/beslogictest/BeslogictestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickeecoco.beslogictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeslogictestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
