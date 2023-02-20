ea71a173624be13b1061833aba57284baaaa531d
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-20 15:51:35 -0500

    Add CrossOrigin annotation

--- BeslogicTest/src/main/java/com/nickeecoco/beslogictest/controller/RentalController.java
+++ BeslogicTest/src/main/java/com/nickeecoco/beslogictest/controller/RentalController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/rental")
 public class RentalController {
     private final RentalService rentalService;
