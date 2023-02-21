ce736f76827d367356af07e5c95290bed6a3144d
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-21 14:38:29 -0500

    Get rental by id, display data on details page

--- BeslogicTestApp/src/app/rental-details/rental-details.component.ts
+++ BeslogicTestApp/src/app/rental-details/rental-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Rental } from '../rental';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RentalService } from '../rental.service';
 
 @Component({
   selector: 'app-rental-details',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./rental-details.component.css']
 })
 export class RentalDetailsComponent {
+  public currentRentalId: any;
+  public rental: Rental | undefined;
 
+  constructor(private rentalService: RentalService, private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.getCurrentRentalId();
+    this.getRental(this.currentRentalId.id);
+  }
+
+  public getCurrentRentalId(): void {
+    this.route.params.subscribe({
+      next: response => this.currentRentalId = response,
+      error: e => alert(e.message)
+    })
+  }
+
+  public getRental(id: number) {
+    this.rentalService.getRentalById(id).subscribe({
+      next: response => this.rental = response,
+      error: e => alert(e.message)
+    })
+  }
 }
