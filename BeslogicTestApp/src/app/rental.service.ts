5e09cd4b3b809683e645082458f24041a21243c4
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-21 12:18:08 -0500

    Correct typo

--- BeslogicTestApp/src/app/rental.service.ts
+++ BeslogicTestApp/src/app/rental.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Rental } from './Rental';
+import { Rental } from './rental';
 
 @Injectable({
   providedIn: 'root'
