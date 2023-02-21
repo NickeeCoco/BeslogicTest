c289b323a5b8f694998fdf1aa1dcd1eafd045222
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-21 12:25:30 -0500

    Display title of rental
    Get rentals from service, test display in component

--- BeslogicTestApp/src/app/rentals/rentals.component.ts
+++ BeslogicTestApp/src/app/rentals/rentals.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Rental } from '../rental';
+import { RentalService } from '../rental.service';
 
 @Component({
   selector: 'app-rentals',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./rentals.component.css']
 })
 export class RentalsComponent {
+  public rentals: Rental[] = [];
 
+  constructor(private rentalService: RentalService) {};
+
+  ngOnInit(): void {
+    this.getRentals();
+  }
+
+  public getRentals(): void {
+    this.rentalService.getRentals().subscribe({
+      next: response => this.rentals = response,
+      error: e => alert(e.message)
+    })
+  }
 }
